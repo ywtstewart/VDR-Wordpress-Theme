257d7a1a604d8699797d1bc2237147bc045df9d2
YWT-XPS14Z\ywtstewart <yuraymar.stewart@gmail.com>
2018-01-24 17:04:21 +0100

    Clean-up homepage.php

--- homepage.php
+++ homepage.php
@@ -8,7 +8,6 @@ Template Name: Home
 <!--- Hero --->
 <div class="hero"
      style="background:url(<?php echo get_theme_mod( 'hero_bg_img', get_bloginfo( 'template_url' ) . '/img/hero-img.jpg' ) ?>); background-size: 100% auto; background-position: center center">
-
 	<div class="container-fluid hero-content-container">
 		<div class="hero-content" data-aos="fade-up">
 			<div class="hero-img">
@@ -23,7 +22,6 @@ Template Name: Home
 				<button class="btn btn--default btn--white"><?php echo nl2br( esc_html( pll__( get_theme_mod( 'hero_btn_2_text', 'Contact' ) ) ) ) ?></button>
 			</a>
 		</div>
-
 	</div>
 </div>
 
@@ -31,30 +29,23 @@ Template Name: Home
 <!--- Over--->
 <section id="over-ons" class="section">
 	<div class="container">
-
 		<div class="content-box col-xs-12 col-md-6 col-md-push-6" data-aos="fade-left">
 			<div class="img-wrapper"><img
 					src="<?php echo get_theme_mod( 'over_img', get_bloginfo( 'template_url' ) . '/img/over-img.jpg' ) ?>"
 					alt="">
 			</div>
 		</div>
-
 		<div class="content-box col-xs-12 col-md-6 col-md-pull-6" data-aos="fade-right">
 			<div class="section__title">
 				<h2><?php echo nl2br( esc_html( pll__( get_theme_mod( 'over_heading', 'Over VDR' ) ) ) ); ?></h2>
                 <h2>Test</h2>
-
 			</div>
 			<p><?php echo nl2br( esc_html( pll__( get_theme_mod( 'over_text', 'This is the default text.' ) ) ) ); ?></p>
-
 			<a href="<?php echo nl2br( esc_html( get_theme_mod( 'over_btn_url', '#contact' ) ) ); ?>">
 				<button class="btn btn--default"><?php echo nl2br( esc_html( pll__( get_theme_mod( 'over_btn_text', 'Contact' ) ) ) ); ?></button>
 			</a>
 		</div>
-
-
 	</div>
-
 </section>
 
 <!--- Diensten --->
@@ -64,10 +55,8 @@ Template Name: Home
 			<div class="section__title">
 				<div class="section__title">
 					<h2><?php echo nl2br( esc_html( pll__( get_theme_mod( 'diensten_heading', 'Diensten' ) ) ) ); ?></h2>
-
 				</div>
 				<p><?php echo nl2br( esc_html( pll__( get_theme_mod( 'diensten_text', 'Content' ) ) ) ); ?></p>
-
 			</div>
 			<div class="section__content">
 				<div class="container boxses">
@@ -75,25 +64,20 @@ Template Name: Home
 						<?php $new_query = new WP_Query( 'post_type=dienst&posts_per_page=-1' );
 						if ( $new_query->have_posts() ) :?>
 							<?php while ( $new_query->have_posts() ) : $new_query->the_post(); ?>
-
 								<div class="col-xs-12 col-sm-4" data-aos="fade-up">
 									<div class="box">
 										<div class="box-img"
 										     style="background-image: url(<?php echo the_post_thumbnail_url() ?>)">
-
 											<h2><? the_title() ?></h2>
 										</div>
 										<div class="box-text">
 											<p><? the_excerpt() ?></p>
-
 										</div>
-
 										<button type="button" class="btn btn-primary" data-toggle="modal"
 										        data-target="#exampleModal"
 										        data-thumbnail="<?php the_post_thumbnail_url() ?>"
 										        data-content="<?php the_content() ?>"
 										        data-title="<?php the_title() ?>"><?php echo nl2br( esc_html( pll__( get_theme_mod( 'diensten_btn_text', 'Lees meer' ) ) ) ); ?></button>
-
 									</div>
 								</div>
 							<?php endwhile; ?>
@@ -111,7 +95,6 @@ Template Name: Home
 	<div class="container">
 		<div class="section__title">
 			<h2><?php echo nl2br( esc_html( pll__( get_theme_mod( 'brands_heading', 'Partners & Brands' ) ) ) ); ?></h2>
-
 		</div>
 		<div class="section-content">
 			<p><?php echo nl2br( esc_html( pll__( get_theme_mod( 'brands_text', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula pretium nisi, sed pulvinar nunc mattis ac. Etiam at suscipit eros. Quisque vitae efficitur leo. Mauris dignissim lorem eu velit consequat malesuada.' ) ) ) ); ?> </p>
@@ -132,12 +115,9 @@ Template Name: Home
 								echo "</a>";
 							}
 						} ?>
-
-
 					<?php endwhile; ?>
 				<?php endif; ?>
 			</div>
-
 		</div>
 	</div>
 </section>
@@ -147,7 +127,6 @@ Template Name: Home
 	<div class="container">
 		<div class="section__title">
 			<h2><?php echo nl2br( esc_html( pll__( get_theme_mod( 'jobs_heading', 'Vacatures' ) ) ) ); ?></h2>
-
 		</div>
 		<div class="section__content">
 			<p><?php echo nl2br( esc_html( pll__( get_theme_mod( 'jobs_text', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula pretium nisi, sed pulvinar nunc mattis ac. Etiam at suscipit eros. Quisque vitae efficitur leo. Mauris dignissim lorem eu velit consequat malesuada.' ) ) ) ); ?> </p>
@@ -160,13 +139,10 @@ Template Name: Home
 							        data-target="#exampleModal" data-thumbnail="<?php echo the_post_thumbnail_url() ?>"
 							        data-content="<?php the_content() ?>"
 							        data-title="<?php the_title() ?>"><?php the_title() ?></button>
-
 						<?php endwhile; ?>
 					<?php else : ?>
 					<?php endif; ?>
-
 				</div>
-
 			</div data-aos="fade-up" >
 			<i class="fa fa-chevron-down arrow-down" data-aos-delay="200" aria-hidden="true"
 			   style="color: #335FB6; font-size:16px;"></i>
@@ -188,10 +164,7 @@ Template Name: Home
 			</div>
 		</div>
 	</div>
-
-
 </section>
-</div>
 
 <!-- /.container -->
 <?php get_template_part( 'modal' );?>
